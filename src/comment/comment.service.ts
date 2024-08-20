bf77dd412d0e4420aa4f5e6b118c64a6d331fff0
argen <argen@MacBook-Pro-Tom.local>
2024-08-20 15:00:12 +0500

    feat: Сделал crud service для comment

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,10 +1,68 @@
+import { CardService } from './../card/card.service';
+import { createCardDto } from 'src/card/dto';
 import { PrismaService } from './../prisma/prisma.service';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CommentService {
     constructor(
-        private readonly prismaService:PrismaService
+        private readonly prismaService:PrismaService,
+        private readonly cardService:CardService
     ){}
 
+    async getOne(commentId:number){
+        return await this.prismaService.comments.findFirst({
+            where: {
+                id: commentId
+            }
+        })
+    }
+    async getAll(cardId:number){
+        return await this.prismaService.comments.findMany({
+            where: {
+                cardsId:cardId
+            }
+        })
+    }
+
+    async save(dto:createCardDto, userId:string, cardId:number){
+        const card = await this.cardService.getOne(cardId)
+        if(!card){
+            throw new BadRequestException()
+        }
+        return await this.prismaService.comments.create({
+            data: {
+                ...dto,
+                usersId: userId,
+                cardsId: cardId
+            }
+        })
+    }
+
+    async updata(dto:createCardDto, commentId:number){
+        const comment = await this.getOne(commentId)
+        if(!comment){
+            throw new BadRequestException()
+        }
+        return await this.prismaService.comments.create({
+            data: {
+                ...dto
+            }
+        })
+    }
+
+    async delete(commentId:number){
+        const comment = await this.getOne(commentId)
+        if(!comment){
+            throw new BadRequestException()
+        }
+        return await this.prismaService.comments.delete({
+            where: {
+                id: commentId
+            },
+            select: {
+                id:true
+            }
+        })
+    }
 }
